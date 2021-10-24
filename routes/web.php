c9cc0ca8fcfbc134d3a586084d3bfb9d29503e9f
Bane Radosavljević <r_bane@hotmail.com>
2021-10-24 09:10:59 +0200

    Episode 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return inertia('Welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home', [
+        'name' => 'Bane',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+        ],
+    ]);
 });
