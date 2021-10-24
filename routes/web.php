c1706248b18e4a4cbdc9756d60c8d4dede03610c
Bane Radosavljević <r_bane@hotmail.com>
2021-10-24 09:53:44 +0200

    Episode 6

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('/users', function () {
 Route::get('/settings', function () {
     return Inertia::render('Settings');
 });
+
+Route::post('/logout', function () {
+    dd('logging the user out');
+});
